40e2d865304369f9ead87f01cabc0fda9e8a2389
3h <1340545944@qq.com>
2020-06-03 08:17:05 +0800

    add time fixing

--- src/getTime.js
+++ src/getTime.js
@@ -1,8 +1,20 @@
 /** @param {number} x */
 const format = x => x.toString().padStart(2, '0');
 
+let deltaTime = 0;
+
+fetch('https://andthetimeis.com/utc/now')
+    .then(res => res.text())
+    .then(serverTime => {
+        deltaTime = (new Date(serverTime)).getTime() - Date.now();
+    })
+    .catch(err => {
+        alert('Failed to fetch current time online! Local time is used instead.');
+        console.error(err);
+    });
+
 export const getTime = () => {
-    const time = new Date();
+    const time = new Date(Date.now() + deltaTime);
     return [time.getHours(), time.getMinutes(), time.getSeconds()]
         .map(format)
         .join(':');
